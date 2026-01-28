ae6c9d70b4b271692e0b6f0ed7a360a25fde8199
KendrickBarrios <kkbarrios@uamv.edu.ni>
2026-01-28 11:24:26 -0600

    refactored assets used across multiple models

---
+++ lib/models/note_asset/physical_exam.g.dart
@@ -0,0 +1,70 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'physical_exam.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PhysicalExam _$PhysicalExamFromJson(Map<String, dynamic> json) => PhysicalExam(
+  pulse: (json['pulse'] as num).toInt(),
+  respiration: (json['respiration'] as num).toInt(),
+  bloodPressure: json['bloodPressure'] as String,
+  temperature: (json['temperature'] as num).toDouble(),
+  weight: (json['weight'] as num).toDouble(),
+  bmi: (json['bmi'] as num).toDouble(),
+  bsa: (json['bsa'] as num).toDouble(),
+  abdominalPerimeter: (json['abdominalPerimeter'] as num).toDouble(),
+  generalAspect: json['generalAspect'] as String,
+  skinAndMucosae: json['skinAndMucosae'] as String,
+  skull: json['skull'] as String,
+  scalp: json['scalp'] as String,
+  eyes: json['eyes'] as String,
+  ears: json['ears'] as String,
+  nose: json['nose'] as String,
+  mouth: json['mouth'] as String,
+  neck: json['neck'] as String,
+  thoracicCavity: json['thoracicCavity'] as String,
+  breast: json['breast'] as String,
+  lungFields: json['lungFields'] as String,
+  cardiac: json['cardiac'] as String,
+  abdomenAndPelvis: json['abdomenAndPelvis'] as String,
+  rectalExam: json['rectalExam'] as String?,
+  upperLimbs: json['upperLimbs'] as String,
+  lowerLimbs: json['lowerLimbs'] as String,
+  urogenital: json['urogenital'] as String?,
+  gynecological: json['gynecological'] as String?,
+  neurological: json['neurological'] as String,
+);
+
+Map<String, dynamic> _$PhysicalExamToJson(PhysicalExam instance) =>
+    <String, dynamic>{
+      'pulse': instance.pulse,
+      'respiration': instance.respiration,
+      'bloodPressure': instance.bloodPressure,
+      'temperature': instance.temperature,
+      'weight': instance.weight,
+      'bmi': instance.bmi,
+      'bsa': instance.bsa,
+      'abdominalPerimeter': instance.abdominalPerimeter,
+      'generalAspect': instance.generalAspect,
+      'skinAndMucosae': instance.skinAndMucosae,
+      'skull': instance.skull,
+      'scalp': instance.scalp,
+      'eyes': instance.eyes,
+      'ears': instance.ears,
+      'nose': instance.nose,
+      'mouth': instance.mouth,
+      'neck': instance.neck,
+      'thoracicCavity': instance.thoracicCavity,
+      'breast': instance.breast,
+      'lungFields': instance.lungFields,
+      'cardiac': instance.cardiac,
+      'abdomenAndPelvis': instance.abdomenAndPelvis,
+      'rectalExam': instance.rectalExam,
+      'upperLimbs': instance.upperLimbs,
+      'lowerLimbs': instance.lowerLimbs,
+      'urogenital': instance.urogenital,
+      'gynecological': instance.gynecological,
+      'neurological': instance.neurological,
+    };
---
+++ lib/models/note_asset/systems_review.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'systems_review.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SystemsReview _$SystemsReviewFromJson(Map<String, dynamic> json) =>
+    SystemsReview(
+      systemicManifestations: json['systemicManifestations'] as String,
+      tegumentarySystem: json['tegumentarySystem'] as String,
+      cardiovascularSystem: json['cardiovascularSystem'] as String,
+      respiratorySystem: json['respiratorySystem'] as String,
+      gastrointestinalSystem: json['gastrointestinalSystem'] as String,
+      urogenitalSystem: json['urogenitalSystem'] as String,
+      neurologicalSystem: json['neurologicalSystem'] as String,
+      musculoskeletalSystem: json['musculoskeletalSystem'] as String,
+    );
+
+Map<String, dynamic> _$SystemsReviewToJson(SystemsReview instance) =>
+    <String, dynamic>{
+      'systemicManifestations': instance.systemicManifestations,
+      'tegumentarySystem': instance.tegumentarySystem,
+      'cardiovascularSystem': instance.cardiovascularSystem,
+      'respiratorySystem': instance.respiratorySystem,
+      'gastrointestinalSystem': instance.gastrointestinalSystem,
+      'urogenitalSystem': instance.urogenitalSystem,
+      'neurologicalSystem': instance.neurologicalSystem,
+      'musculoskeletalSystem': instance.musculoskeletalSystem,
+    };
