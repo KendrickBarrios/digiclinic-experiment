36df2d2a13aa86abb594f9ebbaca0ab597eb5372
KendrickBarrios <kkbarrios@uamv.edu.ni>
2026-01-29 16:36:29 -0600

    created global dependencies and DigiClinicApp as root widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
+import 'package:digiclinic_experiment/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:digiclinic_experiment/services/core/api_client.dart';
+import 'package:digiclinic_experiment/services/core/token_storage.dart';
+import 'package:digiclinic_experiment/services/preferences_storage.dart';
+import 'package:digiclinic_experiment/viewmodels/auth/auth_view_model.dart';
+import 'package:digiclinic_experiment/views/auth_gate.dart';
 
 void main() {
-  runApp(const MainApp());
+
+  final tokenStorage = TokenStorage();
+  final preferencesStorage = PreferencesStorage();
+  final apiClient = ApiClient('http://localhost:8080/api', tokenStorage);
+  final authService = AuthService(apiClient);
+  final authViewModel = AuthViewModel(
+    authService,
+    tokenStorage,
+    preferencesStorage
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthViewModel>(
+          create: (_) => authViewModel
+        )
+      ],
+      child: const DigiClinicApp(),
+    )
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DigiClinicApp extends StatelessWidget {
+
+  const DigiClinicApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'DigiClinic',
+      debugShowCheckedModeBanner: true,
+      home: const AuthGate(),
     );
   }
 }
