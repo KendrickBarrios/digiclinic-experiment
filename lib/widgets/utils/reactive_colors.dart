51153d4e3fa42a8014402e60ebb0824e6750885e
KendrickBarrios <kkbarrios@uamv.edu.ni>
2026-02-07 12:54:21 -0600

    refactore _HoverColors => ReactiveColors as reusable util

---
+++ lib/widgets/utils/reactive_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ReactiveColors {
+
+  const ReactiveColors({
+    required this.background,
+    required this.hoverBackground,
+    required this.pressedBackground,
+    required this.foreGround
+  });
+
+  final Color background;
+  final Color hoverBackground;
+  final Color pressedBackground;
+  final Color foreGround;
+}
