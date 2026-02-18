582921f171d134df39ed6045228e01e182b00c26
KendrickBarrios <kkbarrios@uamv.edu.ni>
2026-02-18 12:38:31 -0600

    ordered imports, turned main to async function to initialize window properties, added CredentialStorage instance to authViewModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,45 @@
-import 'package:digiclinic_experiment/services/clinical_record_service.dart';
-import 'package:digiclinic_experiment/viewmodels/home_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 
-import 'package:digiclinic_experiment/theme/app_theme.dart';
 import 'package:digiclinic_experiment/services/auth/auth_service.dart';
+import 'package:digiclinic_experiment/services/clinical_record_service.dart';
+import 'package:digiclinic_experiment/services/core/credential_storage.dart';
 import 'package:digiclinic_experiment/services/core/api_client.dart';
 import 'package:digiclinic_experiment/services/core/token_storage.dart';
 import 'package:digiclinic_experiment/services/preferences_storage.dart';
+import 'package:digiclinic_experiment/theme/app_theme.dart';
 import 'package:digiclinic_experiment/viewmodels/auth/auth_view_model.dart';
+import 'package:digiclinic_experiment/viewmodels/home_view_model.dart';
 import 'package:digiclinic_experiment/views/auth_gate.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1920, 1080),
+    minimumSize: Size(1280, 720),
+    center: true,
+    title: 'DigiClinic'
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.maximize();
+    await windowManager.show();
+    await windowManager.focus();
+  });
 
   final tokenStorage = TokenStorage();
+  final credentialStorage = CredentialStorage();
   final preferencesStorage = PreferencesStorage();
   final apiClient = ApiClient('http://localhost:8080/api', tokenStorage);
   final authService = AuthService(apiClient);
   final authViewModel = AuthViewModel(
     authService,
     tokenStorage,
+    credentialStorage,
     preferencesStorage
   );
 
