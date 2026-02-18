2b9e8367908ff560cafb79a3dae10e812bf471c4
KendrickBarrios <kkbarrios@uamv.edu.ni>
2026-02-18 12:40:11 -0600

    turned AuthGate to stateful widget to handle auto login function

--- lib/views/auth_gate.dart
+++ lib/views/auth_gate.dart
@@ -6,9 +6,25 @@ import 'package:digiclinic_experiment/viewmodels/auth/auth_status.dart';
 import 'package:digiclinic_experiment/viewmodels/auth/auth_view_model.dart';
 import 'package:digiclinic_experiment/views/login_view.dart';
 
-class AuthGate extends StatelessWidget {
+class AuthGate extends StatefulWidget {
 
   const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      context.read<AuthViewModel>().tryAutoLogin();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
