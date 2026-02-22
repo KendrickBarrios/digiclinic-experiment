e5273c062f450ac2c331d7778972f5136ade20f5
KendrickBarrios <kkbarrios@uamv.edu.ni>
2026-02-22 15:45:34 -0600

    created evaluateStatusCode for use across services

---
+++ lib/services/service_utils.dart
@@ -0,0 +1,44 @@
+enum EndpointType {
+  login,
+  unassociatedResource,
+  byUser,
+  byRecord,
+}
+
+void evaluateStatusCode(int code, EndpointType type, String resource) {
+  String messageStart;
+  String messageComplement;
+
+  switch (code) {
+    case 200:
+    case 201:
+      return;
+    case 400:
+      messageStart = 'Datos incorrectos para solicitud de $resource';
+      break;
+    case 401:
+      messageStart = 'Usuario no autorizado para solicitud de $resource';
+      break;
+    case 404:
+      messageStart = 'No se encontró $resource';
+      break;
+    case 500:
+      messageStart = 'Error del servidor al solicitar $resource';
+      break;
+    default:
+      messageStart = 'Error al solicitar $resource';
+  }
+
+  switch (type) {
+    case EndpointType.byUser:
+      messageComplement = ' del usuario';
+      break;
+    case EndpointType.byRecord:
+      messageComplement = ' del expediente';
+      break;
+    default:
+      messageComplement = '';
+  }
+
+  throw Exception('$messageStart$messageComplement');
+}
